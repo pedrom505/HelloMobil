15e2c54460746ad9d905731d8ab64ff4f42ce79b
pedrom505 <pedrom314@gmail.com>
2018-03-13 23:14:17 -0300

    Created login and validation activity
    - Included the library for "material design of google" in to the build.gradle file
    - Was Change the "ColorPrimary" to blue #00638F
    - Was change the "colorPrimaryDark" to blue #073D54
    - Created the activity "login"
    - Created the activity "validator"
    - Changed the application's icon

---
+++ app/src/main/java/com/decisionmind/pedro/hellomobil/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.decisionmind.pedro.hellomobil.activity;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.decisionmind.pedro.hellomobil.R;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/com/decisionmind/pedro/hellomobil/activity/MainActivity.java
+++ app/src/main/java/com/decisionmind/pedro/hellomobil/activity/MainActivity.java
@@ -1,15 +1,16 @@
-package com.decisionmind.pedro.hellomobil;
+package com.decisionmind.pedro.hellomobil.activity;
 
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.decisionmind.pedro.hellomobil.R;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends Activity {
 
-    private DatabaseReference referenceFirebase = FirebaseDatabase.getInstance().getReference();
+    //private DatabaseReference referenceFirebase = FirebaseDatabase.getInstance().getReference();
 
 
     @Override
@@ -17,7 +18,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        referenceFirebase.child("pontos").setValue(100);
+        //referenceFirebase.child("pontos").setValue(100);
 
     }
 }
