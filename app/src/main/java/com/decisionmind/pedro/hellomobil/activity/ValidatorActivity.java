520442ef446ef19d04f15990fc6c56113c625cbb
pedrom505 <pedrom314@gmail.com>
2018-03-20 22:14:19 -0300

    Token Validation
    - Implemented the mechanism to validate the token send by SMS to user.
    - Modified the Activity validatorActivity. In this modification was change the position of buttom "validar"

--- app/src/main/java/com/decisionmind/pedro/hellomobil/activity/ValidatorActivity.java
+++ app/src/main/java/com/decisionmind/pedro/hellomobil/activity/ValidatorActivity.java
@@ -1,15 +1,56 @@
 package com.decisionmind.pedro.hellomobil.activity;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.decisionmind.pedro.hellomobil.R;
+import com.decisionmind.pedro.hellomobil.helper.Preferences;
+import com.github.rtoshiro.util.format.SimpleMaskFormatter;
+import com.github.rtoshiro.util.format.text.MaskTextWatcher;
 
-public class ValidatorActivity extends AppCompatActivity {
+import java.util.HashMap;
+
+public class ValidatorActivity extends Activity {
+
+    private EditText editText_validationCode;
+    private Button button_validate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_validator);
+
+        editText_validationCode = findViewById(R.id.editText_validationCode);
+        button_validate = findViewById(R.id.button_validate);
+
+        SimpleMaskFormatter simpleMaskValidationCode = new SimpleMaskFormatter("NNNN");
+        MaskTextWatcher maskValidationCode = new MaskTextWatcher(editText_validationCode, simpleMaskValidationCode);
+
+        editText_validationCode.addTextChangedListener( maskValidationCode);
+
+        button_validate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Recovering user preferences data
+                Preferences preference = new Preferences(ValidatorActivity.this);
+                HashMap<String, String> usuario = preference.getUserData();
+
+                String token = usuario.get("token");
+                String tokenTyped = editText_validationCode.getText().toString();
+
+                if(tokenTyped.equals(token)){
+                    Toast.makeText(ValidatorActivity.this, "Token VALIDADO", Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(ValidatorActivity.this, "Token NÃO VALIDADO", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+
     }
 }
